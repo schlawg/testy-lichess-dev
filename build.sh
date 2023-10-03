28eac8f0a15f2b4b04c64d0314d5542d71e738fc
Jonathan Gamble <gamblej@gmail.com>
2023-10-03 09:51:30 -0500

    fix lila fishnet max 100% core usage

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ popd
 cp conf/lila.conf lila/conf/application.conf
 
 # for some reason, lila-fishnet maxes out a core when run with sbt, but it's fine with stage script
-docker compose run --rm --entrypoint "/bin/bash" lila -c "cd lila-fishnet && sbt stage"
+docker compose run --rm --entrypoint "/bin/bash" lila -c "cd /lila-fishnet && sbt stage"
 
 docker-compose up -d
 
