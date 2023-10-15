79a5430f941f2c323a323146aa0f8dc6d064659b
Jonathan Gamble <gamblej@gmail.com>
2023-10-15 16:09:46 -0500

    fah

--- initdb.sh
+++ initdb.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-UID="$(id -u)"
 
-docker compose run --rm -u ${UID:-1003} initdb bash -c \
-  "python3 /lila-db-seed/spamdb/spamdb.py --uri=mongodb://mongodb/lichess $@"
+args="$@"
+
+docker compose run --rm initdb bash -c \
+  "python3 /lila-db-seed/spamdb/spamdb.py --uri=mongodb://mongodb/lichess $args"
 
 docker compose exec mongodb bash -c \
   "mongosh --host mongodb lichess /lila/bin/mongodb/indexes.js"
